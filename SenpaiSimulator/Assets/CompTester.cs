6adf0ac5cd72a89c909ff4d5e585b6f7a0d016f9
Retrograde <flimflamel115@gmail.com>
2017-12-18 20:31:36 -0800

    Backup

---
+++ SenpaiSimulator/Assets/CompTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompTester : MonoBehaviour {
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		Text text = gameObject.AddComponent(typeof(Text)) as Text;
+		
+		text.text = "Hey, good job, pal!";
+	
+	}
+
+
+
+	
+	
+}
---
+++ SenpaiSimulator/Assets/Text Files/TextAssets.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAssets : MonoBehaviour {
+
+    public TextAsset[] textAsset;
+
+    public void TextFiles()
+    {
+        textAsset = new TextAsset[20];
+    }
+}
