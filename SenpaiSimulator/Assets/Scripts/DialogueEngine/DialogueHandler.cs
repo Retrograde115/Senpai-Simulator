b0f97438b71cf49dc21893ff1eb9f4069f352e46
Retrograde <flimflamel115@gmail.com>
2017-12-19 21:36:34 -0800

    Started New Dialogue Engine

---
+++ SenpaiSimulator/Assets/Scripts/DialogueEngine/DialogueHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHandler : MonoBehaviour {
+
+    private Queue<string> sentences;
+    
+    // Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+        
+	}
+
+    public void StartDialogue (Conversation conversation)
+    {
+        Debug.Log("Starting conversation with " + conversation.name);
+    }
+}
---
+++ SenpaiSimulator/Assets/Scripts/DialogueEngine/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public Conversation boxChanScript;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueHandler>().StartDialogue(boxChanScript);
+    }
+
+    
+}
