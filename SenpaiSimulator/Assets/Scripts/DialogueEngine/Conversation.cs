3d5e6429e3f8f8cc4f818e5b6eb4f2fbb569e0f9
Retrograde <flimflamel115@gmail.com>
2017-12-27 00:03:21 -0800

    Second Iteration of Conversation System

--- SenpaiSimulator/Assets/Scripts/DialogueEngine/Conversation.cs
+++ SenpaiSimulator/Assets/Scripts/DialogueEngine/Conversation.cs
@@ -8,6 +8,19 @@ public class Conversation {
     public string name;
 
     [TextArea(3, 10)]
-    public string[] sentences;
+    public List<string> sentences = new List<string>();
+    private string[] assetLines;
+
+    public List<string> ReadFromTextAsset(TextAsset textAsset, int startLine, int endLine)
+    {
+        assetLines = textAsset.text.Split('\n');
+
+        for (int i = startLine; i <= endLine; i++)
+        {
+            sentences.Add(assetLines[i]);
+        }
+
+        return sentences;
+    }
 
 }
--- SenpaiSimulator/Assets/Scripts/DialogueEngine/DialogueHandler.cs
+++ SenpaiSimulator/Assets/Scripts/DialogueEngine/DialogueHandler.cs
@@ -5,27 +5,35 @@ using UnityEngine.UI;
 
 public class DialogueHandler : MonoBehaviour {
 
+    //several of the methods in this class need to be static, but the Unity inspector needs access to their variables, so they all have equivalent duplicate public/static variables
     public Text nameText;
     public Text dialogueText;
     public Animator animator;
     public static Text _nameText;
     public static Text _dialogueText;
     public static Animator _animator;
+    public static int dialogueCounter = 0;
 
     private static Queue<string> sentences;
+
+    //flag meant to be used for game state machine
     public static bool isTalking;
-    
-    // Use this for initialization
-	void Start () {
+
+    //initialize sentence queue
+    void Start () {
         sentences = new Queue<string>();
+	}
+
+    //this method starts the dialogue, using a Conversation class as a constructor
+    public void StartDialogue (Conversation conversation)
+    {
+
+        //initializes public/static variables to be equivalent
         _nameText = nameText;
         _dialogueText = dialogueText;
         _animator = animator;
 
-	}
-
-    public void StartDialogue (Conversation conversation)
-    {
+        //brings the UI to the screen
         animator.SetBool("IsOpen", true);
 
         Debug.Log("Starting conversation with " + conversation.name);
@@ -34,8 +42,10 @@ public class DialogueHandler : MonoBehaviour {
 
         isTalking = true;
 
+        //clears sentence queue just in case there is anything left in there
         sentences.Clear();
 
+        //converts the array of sentences in the Conversation class into a queue of strings
         foreach (string sentence in conversation.sentences)
         {
             sentences.Enqueue(sentence);
@@ -61,5 +71,6 @@ public class DialogueHandler : MonoBehaviour {
         _animator.SetBool("IsOpen", false);
         isTalking = false;
         print("End of conversation.");
+        dialogueCounter++;
     }
 }
